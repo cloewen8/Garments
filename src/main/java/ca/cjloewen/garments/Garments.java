464bfdf870b191e088952908695db09d4fcd99e3
Colton Loewen <loewencjay@outlook.com>
2020-02-24 20:23:21 -0700

    Basic setup

---
+++ src/main/java/ca/cjloewen/garments/Garments.java
@@ -0,0 +1,16 @@
+package ca.cjloewen.garments;
+
+import ca.cjloewen.base.BaseMod;
+import ca.cjloewen.garments.config.ClientConfig;
+import ca.cjloewen.garments.config.ConfigHolder;
+import ca.cjloewen.garments.config.ServerConfig;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Garments.MODID)
+public class Garments extends BaseMod {
+	public static final String MODID = "garments";
+	
+	public Garments() {
+		super();
+	}
+}
