637bd0de568c34b2ab56074a9660a9eddcf667eb
Colton Loewen <loewencjay@outlook.com>
2020-02-28 01:48:38 -0700

    Added loot tables
    Modding has changed so much! Now you need LOOT TABLES for blocks to drop! CRAZY!

---
+++ src/main/java/ca/cjloewen/garments/registries/LootTables.java
@@ -0,0 +1,14 @@
+package ca.cjloewen.garments.registries;
+
+import ca.cjloewen.garments.Garments;
+import net.minecraft.world.storage.loot.LootTableManager;
+
+public class LootTables {
+	public void register() {
+		register("table");
+	}
+	
+	public void register(String name) {
+		//LootTableManager.
+	}
+}
