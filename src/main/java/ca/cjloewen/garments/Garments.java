8f38dfbbdba1f18cf150ef0917ca9c153487122f
Colton Loewen <loewencjay@outlook.com>
2020-03-06 23:37:53 -0700

    Made garments dyable
    I don't want to have to add each garment to a list in order for the dye to show up, so next I'm going to add garments to a list.

--- src/main/java/ca/cjloewen/garments/Garments.java
+++ src/main/java/ca/cjloewen/garments/Garments.java
@@ -7,7 +7,11 @@ import ca.cjloewen.garments.config.ServerConfig;
 import ca.cjloewen.garments.registries.Blocks;
 import ca.cjloewen.garments.registries.Items;
 import ca.cjloewen.garments.registries.Recipes;
+import net.minecraft.item.IDyeableArmorItem;
+import net.minecraftforge.client.event.ColorHandlerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Garments.MODID)
 public class Garments extends BaseMod {
@@ -17,11 +21,17 @@ public class Garments extends BaseMod {
 	public static Items ITEMS;
 	
 	public Garments() {
-		super();
-		
 		CONFIGS = new ConfigHolder<ClientConfig, ServerConfig>(new ClientConfig(), new ServerConfig()).register();
 		BLOCKS = (Blocks) new Blocks().register();
 		ITEMS = (Items) new Items().register();
 		new Recipes().register();
+		FMLJavaModLoadingContext.get().getModEventBus().register(this);
+	}
+	
+	@SubscribeEvent
+	public void onHandleItemColors(ColorHandlerEvent.Item event) {
+		event.getItemColors().register((stack, tintIndex) -> {
+			return tintIndex > 0 ? -1 : ((IDyeableArmorItem)stack.getItem()).getColor(stack);
+		}, Items.UNDERWEAR);
 	}
 }
--- src/main/java/ca/cjloewen/garments/items/garments/Garment.java
+++ src/main/java/ca/cjloewen/garments/items/garments/Garment.java
@@ -1,15 +1,15 @@
 package ca.cjloewen.garments.items.garments;
 
 import ca.cjloewen.garments.registries.Items;
+import net.minecraft.item.IDyeableArmorItem;
 import net.minecraft.item.Item;
 
-public abstract class Garment extends Item {
+public abstract class Garment extends Item implements IDyeableArmorItem {
 	// TODO Lots (a lot based on ArmorItem)!
 	// - Damage over time.
 	// - Dispenser behavior.
 	// - Repair with fabric (damage slowly applied while moving, extra when diving or burning).
 	// - Equip on right click.
-	// - Make garments dyable.
 	
 	public final GarmentSlotType slot;
 	
